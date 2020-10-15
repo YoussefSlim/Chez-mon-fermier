263bf286f062738674d147b9cf6866812d6bbc4c
YoussefSlim <slimyoussef9511@gmail.com>
2020-10-15 20:08:18 +0200

    update src file

---
+++ client/src/components/AtMyFarmer/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.scss';
+
+const AtMyFarmer = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Bienvenue chez mon fermier : "site en cours de construction "
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default AtMyFarmer;
