ffd9d1560e863e45fa43c3353bfc2e6dcc4c898a
Isolia <guibertch@gmail.com>
2020-10-16 15:29:58 +0200

    back-setup router

--- server/app/server.js
+++ server/app/server.js
@@ -4,4 +4,8 @@ const app = express();
 
 const port = process.env.PORT || 5555;
 
+const router = require('./router');
+
+app.use(router);
+
 app.listen(port, () => console.log(`Listening on http://localhost:${port}`)); 
