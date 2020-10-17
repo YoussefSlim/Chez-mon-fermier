7afd69074fc05ba42e1c1ecad5409f0f57183f1f
Jack-Jerry-Stark <jerry.estelle.dumont@gmail.com>
2020-10-17 19:32:44 +0200

    created to productController, no test

---
+++ server/app/controllers/productController.js
@@ -0,0 +1,37 @@
+const { Product, Category } = require('../models');
+
+const productController = {
+
+    productDetail: async (req, res) => {
+        try {
+            const productId = parseInt(req.params.id);
+            const product = await Product.findByPk(productId, {
+                include: [
+                    { association: 'category' },
+                    { association: 'shop' }
+                ]
+            });
+            res.render(req.session.user ? 'cart' : 'product', { product });
+        } catch (error) {
+            console.trace(error);
+            res.status(500).send(error);
+        }
+    },
+
+    listByCategory: async (req, res) =>{
+        try {
+            const categoryId = parseInt(req.params.id);
+            const category = await Category.findByPk(categoryId, {
+                include: [{
+                    association: 'products'
+                }]
+            });
+            const products = category.products;
+            res.render('home', { products });
+        } catch (error) {
+            console.trace(error);
+            res.status(500).send(error);
+        }
+    }
+};
+module.exports = productController;
