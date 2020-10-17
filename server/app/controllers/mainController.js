e3c4e3c0f2ffa633daf361a13a666f84d2bed84b
Etudiant O'clock <etudiant@teleporteur.oclock>
2020-10-17 18:49:55 +0200

    begins of controllers files

---
+++ server/app/controllers/mainController.js
@@ -0,0 +1,20 @@
+const { Product } = require('../models/');
+
+const mainController = {
+
+  homePage: async (req, res) => {
+    try {
+      const products = await Product.findByPk({
+        include: ['category']
+      });
+      res.render('home', { products });
+    } catch (err) {
+      console.trace(err);
+      res.status(500).send(err);
+    }
+  }
+
+};
+
+
+module.exports = mainController;
