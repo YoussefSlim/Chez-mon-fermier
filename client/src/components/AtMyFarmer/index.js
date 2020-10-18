793a2310250138aeb38691cfe006ffdc76bed074
TanguyB17 <tangos@hotmail.fr>
2020-10-18 12:03:39 +0200

    majfooter

--- client/src/components/AtMyFarmer/index.js
+++ client/src/components/AtMyFarmer/index.js
@@ -1,13 +1,29 @@
-import React from 'react';
- import HomePage from '../HomePage'
-import './style.scss';
+import React from "react";
+import HomePage from "../HomePage";
+import Footer from "../Footer";
+import LegalNotice from "../Footer/LegalNotice";
+//import TermsAndConditions from "../Footer/TermsAndConditions";
+//import Confidentiality from "../Footer/Confidentiality";
+//import WhoAreWe from "../Footer/WhoAreWe";
+import Contact from "../Footer/Contact";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import "./style.scss";
 
 const AtMyFarmer = () => {
   return (
-   <>
-    <HomePage />
-   </>
+    <Router>
+      <HomePage />
+      <Footer />
+
+      <Switch>
+        <Route exact path="/" components={HomePage} />
+        <Route path="/Footer" components={Footer} />
+        <Route path="/LegalNotice" components={LegalNotice} />
+        <Route path="/Contact" components={Contact} />
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default AtMyFarmer;
