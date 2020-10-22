d4bfe12a64111ae01aae2a7efd41425e249846a7
Jack-Jerry-Stark <jerry.estelle.dumont@gmail.com>
2020-10-22 15:26:06 +0200

    add test in homepage

--- server/app/controllers/mainController.js
+++ server/app/controllers/mainController.js
@@ -3,7 +3,7 @@ const Product = require('../models/Product');
 const mainController = {
 
   homePage: async (req, res) => {
-    res.json(await Product.findOne(req.params.id));
+    res.json('Bienvenue chezmonfermier.fr');
     }
 
 };
